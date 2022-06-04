efa96f39f504f42a4c60b975c0f6597535186a50
root <root@LAPTOP-OKCRHDJ5.localdomain>
2022-06-04 18:09:21 -0300

    Adding Deploy Script to be run on the runner

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+SOURCE_DIR="/mnt/c/CSCI5308"
+DEPLOY_DIR="/mnt/c/CSCI5308"
+SOURCE_REPO="group-2"
+SOURCE_BRANCH="development"
+DEPLOY_REPO="java-getting-started"
+
+cd $SOURCE_DIR/$SOURCE_REPO
+git pull origin $SOURCE_BRANCH
+cd $DEPLOY_DIR/$DEPLOY_REPO
+cp -r $SOURCE_DIR/$SOURCE_REPO/* $DEPLOY_DIR/$DEPLOY_REPO
+
+git push heroku main
