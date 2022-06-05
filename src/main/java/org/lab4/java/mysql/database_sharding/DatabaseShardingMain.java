087d265d7b3de26711d958d29b8f4af2cba2a1a2
tarashagarwal <tarash.agarwal@gmail.com>
2022-06-05 13:52:09 -0300

    test

---
+++ src/main/java/org/lab4/java/mysql/database_sharding/DatabaseShardingMain.java
@@ -0,0 +1,16 @@
+package org.lab4.java.mysql.database_sharding;
+
+import java.sql.*;
+import java.time.Duration;
+import java.time.Instant;
+
+public class DatabaseShardingMain {
+    public DatabaseShardingMain(){
+    }
+
+    public static void main(String args[]){
+
+        System.out.println("done");
+    }
+
+}
