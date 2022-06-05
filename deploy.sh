8330a97b0162f1dc3c12436c86990ce63312f80b
tarashagarwal <tarash.agarwal@gmail.com>
2022-06-05 15:37:17 -0300

    Updates

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
-SOURCE_DIR="/users/grad/tagarwal/CSCI5308builds"
-DEPLOY_DIR="/users/grad/tagarwal/CSCI5308builds"
+SOURCE_DIR="/mnt/c/CSCI5308"
+DEPLOY_DIR="/mnt/c/CSCI5308"
 SOURCE_REPO="group-2"
 SOURCE_BRANCH="development"
-DEPLOY_REPO="dalcart-production"
+DEPLOY_REPO="java-getting-started"
 
 cd $SOURCE_DIR/$SOURCE_REPO
 git pull origin $SOURCE_BRANCH
