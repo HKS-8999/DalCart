1de8ad335640fc17832e519eaa0cc48d8381f45b
Harsh Shah <hr838264@dal.ca>
2022-07-11 16:55:39 -0300

    Added getUserID() in IUser

--- src/main/java/dalcart/app/models/IUser.java
+++ src/main/java/dalcart/app/models/IUser.java
@@ -6,5 +6,6 @@ import dalcart.app.Repository.IUserPersistence;
 public interface IUser {
 
     IUserPersistence.Result createNewUser(User user,IUserPersistence userPersistence) throws Exception;
+    public Integer getUserID();
 
 }
