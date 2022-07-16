adf475f07baa584b649a33b706ee12b887c7be51
Maitri Manishbhai Savla <mt588638@dal.ca>
2022-07-16 09:39:33 -0300

    Made mock object of Payment

--- src/main/java/dalcart/app/models/IOnlinePayment.java
+++ src/main/java/dalcart/app/models/IOnlinePayment.java
@@ -1,5 +1,7 @@
 package dalcart.app.models;
 
 public interface IOnlinePayment {
+
     public String makePayment(String cardNumber, String cVV, String Expiry);
+
 }
---
+++ src/main/java/mocks/MockOnlinePayment.java
@@ -0,0 +1,43 @@
+package mocks;
+import dalcart.app.models.IOnlinePayment;
+import java.lang.String;
+public class MockOnlinePayment implements IOnlinePayment {
+    public static String cardNum;
+    public static String cvv;
+    public static String exp;
+
+    public static void main(String[] args) {
+
+        String[] cardOne = {"1234567890","112","10/22"};
+        String[] cardTwo = {"0987654321","211","12/23"};
+
+        cardNum = cardOne[0];
+        cvv = cardOne[1];
+        exp = cardOne[2];
+        cardNum = cardTwo[0];
+        cvv = cardTwo[1];
+        exp = cardTwo[2];
+
+        MockOnlinePayment op = new MockOnlinePayment();
+        op.makePayment(cardNum,cvv,exp);
+    }
+
+    @Override
+    public String makePayment(String cardNumber, String cVV, String Expiry) {
+        if (cardNumber.equals("1234567890") && cVV.equals("112") && Expiry.equals("10/22"))
+        {
+            System.out.print("This is a Valid card");
+            return "abc";
+        }
+        else if(cardNum.equals("0987654321") && cvv.equals("211") && exp.equals("12/23"))
+        {
+            System.out.print("This is a Valid card");
+            return "xyz";
+        }
+        else
+        {
+            System.out.print("The card details doesnot match");
+            return "pqr";
+        }
+    }
+}
