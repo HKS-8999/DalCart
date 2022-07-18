c9ddb80c8278c35f02003205a1710a175bbec4ce
Harsh Shah <hr838264@dal.ca>
2022-07-18 17:13:58 -0300

    Changed Abstract class implemented Getters and Setters in abstract class only

--- src/main/java/dalcart/app/models/User.java
+++ src/main/java/dalcart/app/models/User.java
@@ -7,36 +7,6 @@ import java.util.Locale;
 
 public class User extends IUser {
 
-    public int getUserId(){return userID;}
-
-    public void setUserId(int userID){ this.userID = userID; }
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-    public String getMobileNo() {
-        return mobileNo;
-    }
-
     @Override
     public void loadUserAttributes(IUserPersistence userPersistence) {
         IUser user = userPersistence.loadUserAttributesbyUsername(this.email);
@@ -49,41 +19,14 @@ public class User extends IUser {
         this.setDesignation(user.getDesignation());
     }
 
-    public void setMobileNo(String mobileNo) {
-        this.mobileNo = mobileNo;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getEmail() {
-        return email;
-    }
 
     public IUserPersistence.Result createNewUser(IUser user, IUserPersistence userPersistence) throws Exception {
         this.userID = userPersistence.save(user);
         return IUserPersistence.Result.SUCCESS;
     }
-    public Integer getUserID(){
-        return this.userID;
-    }
-
-    public void setUserID(int userID){
-        this.userID = userID;
-    }
 
     public Security.RESULT hasAccess(Security security, IUser user){
         return security.authenticateUser(user);
-    }
-
-
-    public String getDesignation() {
-        return designation;
-    }
-
-    public void setDesignation(String designation) {
-        this.designation = designation;
     }
 
     public boolean isAdmin(String designation){
