3f7e742eb955afda3a33db80772de296ec9ad432
tarashagarwal <tarash.agarwal@gmail.com>
2022-07-22 14:14:54 -0300

    Updating Product Quantity if Already Added to Cart

--- src/main/java/dalcart/app/Repository/OrderProducts.java
+++ src/main/java/dalcart/app/Repository/OrderProducts.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Repository;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 @Repository
 public
@@ -13,11 +14,31 @@ class OrderProducts  {
     static PreparedStatement preparedStatement;
 
     public static boolean saveOrderProduct(Integer orderId, Integer productId){
-        String query = "insert into order_products (order_id, product_id) values (?,?);";
         try {
-            preparedStatement = ConnectionManager.getInstance().getConnection().prepareStatement(query);
+            String findQuery = "select * from order_products where order_id = ? and product_id = ?";
+            String query = "";
+            preparedStatement = ConnectionManager.getInstance().getConnection().prepareStatement(findQuery);
             preparedStatement.setInt(1, orderId);
             preparedStatement.setInt(2, productId);
+            ResultSet result = preparedStatement.executeQuery();
+
+            if(result.next()){
+                System.out.println("Product Found");
+                query = "update order_products set product_quantity = (product_quantity + 1) where order_id = ? and product_id = ?";
+                preparedStatement = ConnectionManager.getInstance().getConnection().prepareStatement(query);
+                preparedStatement.setInt(1, orderId);
+                preparedStatement.setInt(2, productId);
+            }else{
+                System.out.println("Product Not Found");
+                query = "insert into order_products (order_id, product_id, product_quantity) values (?,?,?);";
+                preparedStatement = ConnectionManager.getInstance().getConnection().prepareStatement(query);
+                preparedStatement.setInt(1, orderId);
+                preparedStatement.setInt(2, productId);
+                preparedStatement.setInt(3, 1);
+            }
+
+            System.out.println("Updating");
+
             preparedStatement.executeUpdate();
         }catch(Exception e){
             e.printStackTrace();
